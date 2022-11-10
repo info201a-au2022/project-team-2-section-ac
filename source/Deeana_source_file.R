6702e62b45d0064aed0a2d963b780920f0daa191
Deeana Nasrulai <deeananasrulai@Deeanas-MacBook-Pro.local>
2022-11-10 14:58:28 -0800

    map done

--- source/Deeana_source_file.R
+++ source/Deeana_source_file.R
@@ -7,8 +7,10 @@ library(ggmap)
 library(dplyr)
 library(tidyverse)
 
+
 results_df <- read.csv("https://raw.githubusercontent.com/martj42/international_results/master/results.csv")
 View(results_df)
+library(results_df)
 
 new_df <- data.frame(results_df$home_team, results_df$home_score, 
                     results_df$away_team, results_df$away_score)
@@ -37,6 +39,45 @@ total_goals_by_team <- df %>%
   rename(home.goals = score.x) %>% 
   rename(away.goals = score.y) %>% 
   mutate(total_goals = home.goals + away.goals)
-  
+
+mapData <- total_goals_by_team
+View(mapData)
+
+#world map set up code
+WorldData=map_data("world")  #to take the world data
+str(WorldData) #to see the structure of the world data
+View(WorldData)
+WorldData[WorldData == "USA"] <- "United States"
+WorldData[WorldData == "China"] <- "China PR"
+Combined <- WorldData[mapData$team %in% mapData$team, ]
+Combined$value <- mapData$total_goals[match(Combined$region, mapData$team)]
+
+Countries <- unique(Combined$region)
+CDF <- data.frame(label1=Countries)
+for(i in CDF) {
+  Combined$value <- ifelse(Combined$region %in% CDF$label1[i], 
+                           (mapData$total_goals), Combined$team) 
+}
+
+#map aesthetics code
+worldMap1 <- ggplot(Combined, aes(x=long, y= lat, group = group, fill = value)) +
+  geom_polygon(color = 'white') +
+  scale_fill_continuous(low = 'pale green', high = 'black', guide = 'colorbar') +
+  theme_bw() +
+  labs(fill = "Goals Scored", title = 'Density of Goals Scored by 
+       National Football Teams (1872-2022)', x ='', y = '') +
+  scale_y_continuous(breaks = c()) +
+  scale_x_continuous(breaks = c()) +
+  theme(panel.border = element_blank())
+#interactive element code
+ggplotly(worldMap1) %>%
+  highlight(
+    "plotly_hover",
+    selected = attrs_selected(line = list(color = "black"), 
+            )
+  ) %>%
+  widgetframe::frameWidget()
+
+
   
 
