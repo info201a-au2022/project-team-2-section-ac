42ab7bdff87593e3ffda5cdd6c7e252f6b503e4c
Deeana Nasrulai <deeananasrulai@Deeanas-MacBook-Pro.local>
2022-11-12 12:29:33 -0800

    debugging

--- source/table.sourcefile.R
+++ source/table.sourcefile.R
@@ -3,7 +3,5 @@ library(dplyr)
 library(tidyverse)
 library(knitr)
 results_df <- read.csv("https://raw.githubusercontent.com/martj42/international_results/master/results.csv")
-View(results_df)
-
 
 kable(results_df)
