3ba134ddf4999940767a3c706aae78312aba9168
Deeana Nasrulai <deeananasrulai@Deeanas-MacBook-Pro.local>
2022-12-01 15:51:52 -0800

    started

--- final/app/app_server.R
+++ final/app/app_server.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(shiny)
 library(plotly)
 library(ggplot2)
+library(leaflet)
 
 results_df <- read.csv("https://raw.githubusercontent.com/martj42/international_results/master/results.csv")
 
@@ -37,4 +38,59 @@ server <- function(input, output) {
               scale_fill_manual(values = c("#00A36C", "darkgreen")) + 
               labs(title = "Home Wins vs. Away Wins")
   })
+}
+
+#Deeana's code
+new_df <- data.frame(results_df$home_team, results_df$home_score, 
+                     results_df$away_team, results_df$away_score)
+
+home <- new_df %>% 
+  rename(score = results_df.home_score) %>% 
+  rename(team = results_df.home_team) %>%
+  group_by(team,score) %>%
+  aggregate(score ~ team, sum)
+away <- new_df %>% 
+  rename(score = results_df.away_score) %>% 
+  rename(team = results_df.away_team) %>% 
+  group_by(team, score) %>% 
+  aggregate(score ~ team, sum)
+df <- merge(home, away, by = 'team')
+total_goals_by_team <- df %>% 
+  rename(home.goals = score.x) %>% 
+  rename(away.goals = score.y) %>% 
+  mutate(total_goals = home.goals + away.goals)
+mapData <- total_goals_by_team
+WorldData=map_data("world")  #to take the world data
+str(WorldData) #to see the structure of the world data
+WorldData[WorldData == "USA"] <- "United States"
+WorldData[WorldData == "China"] <- "China PR"
+WorldData[WorldData == "Democratic Republic of the Congo"] <- "DR Congo"
+Combined <- WorldData[mapData$team %in% mapData$team, ]
+Combined$value <- mapData$total_goals[match(Combined$region, mapData$team)]
+Countries <- unique(Combined$region)
+CDF <- data.frame(label1=Countries)
+for(i in CDF) {
+  Combined$value <- ifelse(Combined$region %in% CDF$label1[i], 
+                           (mapData$total_goals), Combined$value) 
+  
+}
+
+server <- function(input, output) {
+  
+
+ output$worldMap1 <- renderPlot({ 
+   ggplot(Combined, aes(x=long, y= lat, group = group, 
+                                             fill = value) +
+     geom_polygon(color = 'white') +
+     scale_fill_continuous(low = 'pale green', high = 'black', 
+                           guide = 'colorbar') +
+     theme_bw() +
+     labs(fill = "Goals Scored", title = 'Density of Goals Scored by 
+       National Football Teams (1872-2022)', x ='', y = '') +
+     scale_y_continuous(breaks = c()) +
+     scale_x_continuous(breaks = c()) +
+     theme(panel.border = element_blank())
+   )
+ })
+ 
 }
