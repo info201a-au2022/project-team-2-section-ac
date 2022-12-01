bbd382c24db55aa99dfd4deef763146a24045f66
ShiraZur <szur@uw.edu>
2022-12-01 13:24:32 -0800

    started my part

--- final/app/app_server.R
+++ final/app/app_server.R
@@ -1,14 +1,40 @@
 library(dplyr)
-#library(shiny)
+library(shiny)
 library(plotly)
 library(ggplot2)
 
 results_df <- read.csv("https://raw.githubusercontent.com/martj42/international_results/master/results.csv")
 
+away_wins_ratio_plot <- results_df %>%
+  mutate(team = away_team) %>%
+  mutate(year = paste(substr(date, 1, 3), "0", sep = "")) %>%
+  group_by(year) %>%
+  mutate(away_wins = (away_score > home_score)) %>% 
+  summarize(away_games=n(), away_wins=sum(away_wins)) %>%
+  mutate(ratio = (away_wins / away_games)) %>%
+  mutate(location = "Away") %>%
+  select(year, ratio, location)
+
+home_wins_ratio_plot <- results_df %>% # I did the same thing as the above data but for the home wins ratio.
+  mutate(team = home_team) %>%
+  mutate(year = paste(substr(date, 1, 3), "0", sep = "")) %>%
+  group_by(year) %>%
+  mutate(home_wins = (away_score < home_score)) %>% 
+  summarize(home_games=n(), home_wins=sum(home_wins)) %>%
+  mutate(ratio = (home_wins / home_games)) %>%
+  mutate(location = "Home") %>%
+  select(year, ratio, location)
+
+total_ratio_plot <- union(away_wins_ratio_plot, home_wins_ratio_plot)
+
 server <- function(input, output) {
-  #Nathan
   
-  #Deeana
-  
-  #Shira
+  output$total_ratio_plot <- renderPlot({
+    
+    ggplot(total_ratio_plot, 
+           main = input$year, aes(x = year, y = ratio, fill = location)) + 
+              geom_bar(stat = "identity", position = "dodge") + 
+              scale_fill_manual(values = c("#00A36C", "darkgreen")) + 
+              labs(title = "Home Wins vs. Away Wins")
+  })
 }
--- final/app/app_ui.R
+++ final/app/app_ui.R
@@ -5,14 +5,42 @@ library(ggplot2)
 
 results_df <- read.csv("https://raw.githubusercontent.com/martj42/international_results/master/results.csv")
 
-introduction_page <- tabPanel(
-  "Introduction",
-  p("This is the introduction for the soccer app.")
+# introduction_page <- tabPanel(
+#   "Introduction",
+#   p("This is the introduction for the soccer app.")
+# )
+# 
+# nathans_page <- tabPanel(
+#   "Nathan's Analysis",
+#   p("This is the interactive graph for comparing two teams performance"),
+# )
+
+shiras_page <- tabPanel(
+  fluidPage(    
+    
+    titlePanel("Home Goals vs. Away Goals"),
+    
+    sidebarLayout(      
+      
+      sidebarPanel(
+        selectInput("year", "Year:", 
+                    choices=total_ratio_plot$year),
+        hr(),
+        helpText("Data from International Football results.")
+      ),
+      
+      mainPanel(
+        plotOutput("total_ratio_plot")  
+      )
+      
+    )
+  )
 )
 
-nathans_page <- tabPanel(
-  "Nathan's Analysis",
-  p("This is the interactive graph for comparing two teams performance"),
+report_page <- tabPanel(
+  "Report Page",
+  titlePanel("Report Page"),
+  p("From looking at the", em("International Football Results from 1872 to 2022"), "results, we have made a lot of interesting discoveries. Firstly, we created a map visualization that revealed the number of goals scored per country. This goal density visualization allowed us to see a big picture overview of national teams’ performance. We figure that this type of analysis can help the national teams themselves, but also the players and supporters as they can be informed on the status of their team’s success. Secondly, we created a home goal vs. away goal bar chart over time. The bar chart helped prove the", em("home game advantage theory"), "-- a theory that states that teams that play a match in their home city are more likely to win than teams that play a match away, at a city they are not from. We figure that this type of analysis can help team managers and coaches that are worried that the home game advantage may no longer serve them; they can refer to this analysis and see that when they play home, they will be much more likely to win, and therefore need to prepare their teams when they play away. Lastly, we created a line graph analysis of two teams -- Scotland and England -- to show how two teams can be compared and to inspire similar comparison analysis in the future. We figure this type of information can help coaches, fans, and investors make decisions about which team to support financially. Overall, our project reveals the various and interesting ways that soccer data can be observed and analyzed.")
 )
 
-ui <- navbarPage("Title", introduction_page, nathans_page)
+ui <- navbarPage("AC-2 Project", shiras_page, report_page)
